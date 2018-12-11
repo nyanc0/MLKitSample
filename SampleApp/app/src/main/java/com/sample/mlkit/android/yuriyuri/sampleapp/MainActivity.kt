5b2c7ace69aa5bca4f7e8ea77500c44bef35e3c5
福岡 侑里奈 <y_fukuoka@waku-2.com>
2018-12-11 19:20:16 +0900

    add view

--- SampleApp/app/src/main/java/com/sample/mlkit/android/yuriyuri/sampleapp/MainActivity.kt
+++ SampleApp/app/src/main/java/com/sample/mlkit/android/yuriyuri/sampleapp/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.sample.mlkit.android.yuriyuri.sampleapp
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import com.sample.mlkit.android.yuriyuri.sampleapp.R
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import com.sample.mlkit.android.yuriyuri.sampleapp.databinding.ActivityMainBinding
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(binding.bottomAppBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        MenuInflater(this).inflate(R.menu.navigation, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            // TODO：デバイスとクラウドの切り替え
+            R.id.navigation_cloud -> Toast.makeText(this, "Cloud is Clicked", Toast.LENGTH_SHORT).show()
+            R.id.navigation_device -> Toast.makeText(this, "Device is Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
