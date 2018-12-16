9ed75350c46be6f2243be237df7829e897d37b47
yurina fukuoka <r0lling.beat193@gmail.com>
2018-12-17 00:15:00 +0900

    add camera view

--- SampleApp/app/src/main/java/com/sample/mlkit/android/yuriyuri/sampleapp/CameraActivity.kt
+++ SampleApp/app/src/main/java/com/sample/mlkit/android/yuriyuri/sampleapp/CameraActivity.kt
@@ -3,38 +3,29 @@ package com.sample.mlkit.android.yuriyuri.sampleapp
 import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
 import com.sample.mlkit.android.yuriyuri.sampleapp.databinding.ActivityCameraBinding
 import dagger.android.support.DaggerAppCompatActivity
 
-class CameraActivity : DaggerAppCompatActivity() {
+class CameraActivity : DaggerAppCompatActivity(), View.OnClickListener {
+
+    // https://github.com/google/cameraview/blob/master/demo/src/main/java/com/google/android/cameraview/demo/MainActivity.java
 
     private val binding: ActivityCameraBinding by lazy {
         DataBindingUtil.setContentView<ActivityCameraBinding>(this, R.layout.activity_camera)
     }
 
-    override fun onStart() {
-        super.onStart()
-        binding.cameraView.onStart()
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
     }
 
-    override fun onResume() {
-        super.onResume()
-        binding.cameraView.onResume()
-    }
-
-    override fun onPause() {
-        binding.cameraView.onPause()
-        super.onPause()
-    }
-
-    override fun onStop() {
-        binding.cameraView.onStop()
-        super.onStop()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        binding.cameraView.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.take_picture -> binding.camera.takePicture()
+        }
     }
 
     companion object {
